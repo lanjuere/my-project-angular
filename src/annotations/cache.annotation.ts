f81b711254dd347359eef2e6d0e43099a8b9c463
lanjuere <lionel.anjuere@gmail.com>
2019-04-10 17:40:43 +0200

    Simplification avec AsyncSubject

--- src/annotations/cache.annotation.ts
+++ src/annotations/cache.annotation.ts
@@ -1,9 +1,8 @@
-import {Observable} from "rxjs";
+import {Observable,AsyncSubject} from "rxjs";
 // Define a function with one parameter: the key under which the data will be cached
 export function CacheReturnValue(key: string) {
   // Return our high-order function
   let observers = {};
-  let exposedObersvers = {};
   let results = {};
   let TIMEOUT_KEY = "TIMEOUT_KEY";
   return function(target, methodName, descriptor) {
@@ -16,45 +15,22 @@ export function CacheReturnValue(key: string) {
       let self = this;
       // Execute the method with its initial context and arguments
       // Return value is stored into a variable instead of being passed to the execution stack
-
-      let result = JSON.parse(sessionStorage.getItem(key)) ?  JSON.parse(sessionStorage.getItem(key)) : results[key];
-      function sequenceObserver(observer){
-        if(!result){
-          
-          if(!observers[key]){
+      let result = JSON.parse(sessionStorage.getItem(key));
+      if(!observers[key]){
             console.log("oserver : ", observers[key]);
-            observers[key] = <Observable<any>>originalMethod.apply(self, args);
-
-            observers[key].subscribe(function(res){
-              
+            observers[key] = new AsyncSubject<any>();
+            <Observable<any>>originalMethod.apply(self, args).subscribe(function(res){
               results[key] = res;
               sessionStorage.setItem(key,JSON.stringify(res));
-              exposedObersvers[key].forEach(function(obs){
                 setTimeout(()=>{
-                  obs.next(res); 
-                  obs.complete();
-                },JSON.parse(sessionStorage.getItem(TIMEOUT_KEY)) );
-                          
+                  observers[key].next(res); 
+                  observers[key].complete();
+                },JSON.parse(sessionStorage.getItem(TIMEOUT_KEY)) );         
               });
-              
-            }); 
-          }
-          if(!exposedObersvers[key]){
-            exposedObersvers[key] = [];
-          } 
-          
-          exposedObersvers[key].push(observer);
-          observer.id = exposedObersvers[key].length;
-        }else {
-          observer.next(result);
-          observer.complete();
-        }
-        return {unsubscribe() {}};
       }
-
     
       // Return back the value to the execution stack
-      return new Observable(sequenceObserver);
+      return observers[key];
     };
 
     // Return the descriptorwith the altered value
